ff12e16c9c8b56e37f838c0b181f8ae4d0f07a94
HettieC <hester.chapman@btinternet.com>
2025-02-21 15:43:11 +0100

    unidirectional reactions

--- wip.jl
+++ wip.jl
@@ -26,20 +26,23 @@ model.reactions["biomass"] = CM.Reaction(
         "CHEBI:43474" => 1, #phosphate
         "CHEBI:15378" => 1, #h+
         "CHEBI:456216" => 1, #adp
-        # "CHEBI:46398" => -0.1,   #UTP
-        # "CHEBI:37565" => -0.059,   #GTP
-        # "CHEBI:37563" => -0.059,   #CTP       
-        # "CHEBI:57692" => -0.007,    #FAD  
-        # "CHEBI:61404" => -0.02,    #dATP
-        # "CHEBI:57287" => -4.42e-5, #CoA
-        # "CHEBI:37568" => -0.02,    #dTTP
-        # "CHEBI:61429" => -0.099,   #dGTP
-        # "CHEBI:61481" => -0.099,   #dCTP
-        #
-        # "CHEBI:57783" => -2e-5, #NADPH 
-        # "CHEBI:57945" => -2e-5, #NADH
-        # "CHEBI:58349" => -2e-5, #NADP(+)
-        # "CHEBI:57540" => -2e-5, #NAD(+)
+        
+        "CHEBI:46398" => -0.1,   #UTP
+        "CHEBI:37565" => -0.059,   #GTP
+        "CHEBI:37563" => -0.059,   #CTP       
+        #"CHEBI:57692" => -0.007,    #FAD  
+        "CHEBI:58453" => -0.007,    #
+
+        "CHEBI:61404" => -0.02,    #dATP
+        "CHEBI:57287" => -4.42e-5, #CoA
+        "CHEBI:37568" => -0.02,    #dTTP
+        "CHEBI:61429" => -0.099,   #dGTP
+        "CHEBI:61481" => -0.099,   #dCTP
+        
+        "CHEBI:57783" => 2e-5, #NADPH 
+        "CHEBI:57945" => 2e-5, #NADH
+        "CHEBI:58349" => -2e-5, #NADP(+)
+        "CHEBI:57540" => -2e-5, #NAD(+)
 
         # "CHEBI:30807" => -1.0,    #tetradecanoate
         # "CHEBI:25646" => -1.0,    #octanoate
@@ -50,17 +53,17 @@ model.reactions["biomass"] = CM.Reaction(
         # "CHEBI:32682" => -0.111,  #L-arginine  
         # "CHEBI:57762" => -0.207,  #L-valine  
         # "CHEBI:60039" => -0.116,  #L-proline
-        # "CHEBI:35235" => -0.019,  #L-cysteine
+        "CHEBI:35235" => -0.019,  #L-cysteine
         #"CHEBI:57305" => -0.19,  #glycine
         #"CHEBI:33384" => -0.198, #L-serine         
         #"CHEBI:29991" => -0.261, #L-aspartate
-        #"CHEBI:57972" => -0.212, #L-alanine
+        "CHEBI:57972" => -0.212, #L-alanine
         #"CHEBI:58359" => -0.2,   #L-glutamine
         # #"CHEBI:57912" => -1.0,   #L-tryptophan
         #"CHEBI:29985" => -1.0,   #L-glutamate
         #"CHEBI:32551" => -0.235, #L-lysine
         # #"CHEBI:57844" => -0.084, #L-methionine
-        #"CHEBI:58045" => -0.269, #L-isoleucine
+        "CHEBI:58045" => -0.269, #L-isoleucine
         #"CHEBI:57595" => -0.073, #L-histidine
         #"CHEBI:57926" => -0.179, #L-threonine
         #"CHEBI:58095" => -0.137, #L-phenylalanine
@@ -79,20 +82,22 @@ model.reactions["biomass"] = CM.Reaction(
     notes=Dict("ref" => ["Diaz Calvo, S. epidermis, Metabolites 2022"]),
 )
 
-
 fba_sol = parsimonious_flux_balance_analysis(model; optimizer=HiGHS.Optimizer)
 
 ex_fluxes = Dict(
     ("CHEBI:$(split(string(x),"EX_")[2])", model.metabolites["CHEBI:$(split(string(x),"EX_")[2])"].name,x) => y
-    for (x, y) in fba_sol.fluxes if startswith(string(x), "EX") && abs(y)>1e-5
+    for (x, y) in fba_sol.fluxes if startswith(string(x), "EX") && y<-1e-5
 )
+
+["CHEBI:$(split(string(x),"EX_")[2]),$(model.metabolites["CHEBI:$(split(string(x),"EX_")[2])"].name)" for (x, y) in fba_sol.fluxes if startswith(string(x), "EX") && y<-1e-5]
+
+Dict(x=>y for (x,y) in fba_sol.fluxes if abs(y)>1e-5)
 
 open("data/fluxes.json", "w") do io
     JSON.print(io, Dict(string(x) => y for (x, y) in fba_sol.fluxes))
 end
 
 
-delete!(model.reactions, "EX_29969")
 ### make sinks 
 for (m, met) in model.metabolites
     haskey(model.reactions, "EX_$(split(m,':')[2])") && continue
@@ -113,9 +118,6 @@ for (m, met) in model.metabolites
         delete!(model.reactions, "EX_$(split(m,':')[2])")
     end
 end
-
-
-
 
 
 fba_sol = parsimonious_flux_balance_analysis(model; optimizer=HiGHS.Optimizer)
