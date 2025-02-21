3dedfc7752ae9d25e94052faf543018f88fbb6a7
HettieC <hester.chapman@btinternet.com>
2025-02-21 15:43:11 +0100

    unidirectional reactions

--- wip.jl
+++ wip.jl
@@ -80,17 +80,49 @@ model.reactions["biomass"] = CM.Reaction(
     notes=Dict("ref" => ["Diaz Calvo, S. epidermis, Metabolites 2022"]),
 )
 
-
 fba_sol = parsimonious_flux_balance_analysis(model; optimizer=HiGHS.Optimizer)
 
 ex_fluxes = Dict(
     ("CHEBI:$(split(string(x),"EX_")[2])", model.metabolites["CHEBI:$(split(string(x),"EX_")[2])"].name,x) => y
-    for (x, y) in fba_sol.fluxes if startswith(string(x), "EX") && abs(y)>1e-5
+    for (x, y) in fba_sol.fluxes if startswith(string(x), "EX") && y<-1e-5
 )
+
+["CHEBI:$(split(string(x),"EX_")[2]),$(model.metabolites["CHEBI:$(split(string(x),"EX_")[2])"].name)" for (x, y) in fba_sol.fluxes if startswith(string(x), "EX") && y<-1e-5]
+
+Dict(x=>y for (x,y) in fba_sol.fluxes if abs(y)>1e-5)
 
 open("data/fluxes.json", "w") do io
     JSON.print(io, Dict(string(x) => y for (x, y) in fba_sol.fluxes))
 end
+
+### make sinks 
+for (m, met) in model.metabolites
+    haskey(model.reactions, "EX_$(split(m,':')[2])") && continue
+    model.reactions["EX_$(split(m,':')[2])"] = CM.Reaction(; stoichiometry=Dict(m => 1), notes=Dict("reason" => ["added as sink"]), upper_bound=0.0)
+end
+
+
+fba_sol = parsimonious_flux_balance_analysis(model; optimizer=HiGHS.Optimizer)
+
+
+
+
+
+### delete unneeded sinks 
+
+for (m, met) in model.metabolites
+    if abs(fba_sol.fluxes["EX_$(split(m,':')[2])"]) < 1e-5
+        delete!(model.reactions, "EX_$(split(m,':')[2])")
+    end
+end
+
+
+
+
+
+fba_sol = parsimonious_flux_balance_analysis(model; optimizer=HiGHS.Optimizer)
+
+
 
 
 Dict(escher_model.reactions[string(x)].name=>y for (x,y) in fba_sol.fluxes if abs(y)>1e-5 && string(x)!="biomass")
