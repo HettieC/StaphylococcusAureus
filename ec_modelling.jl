071c4c9feba617f291b89da40992718dc148fe85
nastasia-sw <schwan.nastasia@gmx.de>
2025-02-26 11:34:04 +0100

    adding isozymes

--- ec_modelling.jl
+++ ec_modelling.jl
@@ -6,5 +6,16 @@ rxns = DataFrame(CSV.File("data/model/metabolic_reactions.csv"))
 select!(rxns, :KEGG_ID, :RHEA_ID, :Protein)
 
 df = leftjoin(rxns,iso, on = [:Protein, :KEGG_ID])
-CSV.write("df.csv", df)
+CSV.write("reaction_isozymes.csv", df)
 
+### get reaction turnover numbers kcat
+
+### load reaction_isozymes
+
+### change units of kcat 
+
+### add enzyme molar masses 
+
+### total enzyme capacity
+
+### run enzyme_constrained_flux_balance_analysis
